4acd572cbce5aa9afaadaf79827346b52810fa79
Alex Aheyev <ah.alesha@gmail.com>
2017-02-25 01:18:05 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+package pkg9;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog= new Dog();
+        Human human = new Human();
+        cat.getSound();
+        dog.getSound();
+        human.getSound();
+    }
+    
+}
